ad4aa6c0cd1e49ea89b796cb0b15548eeff121b9
Marconi Afonso <77650431+kaduzitoh@users.noreply.github.com>
2021-09-27 14:09:39 -0300

    Servidor e Mailing

---
+++ src/views/Listas/ListaServidores.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="relatorios">
+    <PagesSubHeader titulo="Lista de servidores" icon="fal fa-list" >
+      <div class="card">
+        <div class="card-body"/>
+      </div>
+      <form class="container">
+        <div class="form-group">
+          <div class="row">
+            <div class="col-7 col-inputs">
+              <div class="input-group d-flex">
+
+                <input
+                    aria-describedby="basic-addon1"
+                    aria-label="Username"
+                    class="form-control"
+                    placeholder=""
+                    type="text"
+                    v-on:input="filter = $event.target.value"
+                />
+              </div>
+            </div>
+
+            <div class="col-2 col-botoes">
+              <button class="btn btn-info waves-effect waves-themed"  type="submit"><i
+                  class="fal fa-search"></i></button>
+
+            </div>
+            <div class="col-1 col-inputs mr-4">
+              <div
+                  class="
+
+                                      custom-control custom-switch
+                                      border border-0
+                                      mt-1
+                                    "
+              >
+                <input
+                    id="customSwitch1"
+                    class="custom-control-input bg-dark"
+                    type="checkbox"
+
+                />
+                <label
+                    class="custom-control-label"
+                    for="customSwitch1"
+                ></label>
+              </div>
+
+            </div>
+            <div class="col-1 col-botoes">
+              <router-link :to="{ name: 'RegistroUsuarios' }">
+                <button class="btn btn-success waves-effect waves-themed dow-color" name="pesquisa-faturamento"
+                        type="submit"><i class="fal fa-plus"></i></button>
+              </router-link>
+            </div>
+
+          </div>
+        </div>
+      </form>
+    </PagesSubHeader>
+    <div class="panel ">
+      <div class="panel-container show">
+        <div class="panel-content">
+          <TabelaServidores/>
+        </div>
+      </div>
+    </div>
+    <!-- <table class="table table-hover">
+      <thead>
+        <tr class="thead-dark">
+          <th scope="col">Usuário</th>
+          <th scope="col">Nome</th>
+          <th scope="col">E-mail</th>
+          <th scope="col">Perfil</th>
+          <th scope="col">Status</th>
+          <th scope="col">Ação</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Exemplo</th>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>
+            <div class="custom-control custom-switch">
+              <input
+                type="checkbox"
+                class="custom-control-input"
+                id="customSwitch3"
+                disabled
+              />
+              <label
+                class="custom-control-label"
+                for="customSwitch3"
+              ></label>
+            </div>
+          </td>
+          <td>
+            <router-link :to="{ name: 'Registro' }">
+            <i class="fal fa-pencil d-inline"></i>
+            </router-link>
+            <i class="fal fa-trash-alt d-inline ml-2"></i>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Outro Exemplo</th>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>
+            <div class="custom-control custom-switch">
+              <input
+                type="checkbox"
+                class="custom-control-input"
+                id="customSwitch2"
+                disabled
+              />
+              <label
+                class="custom-control-label"
+                for="customSwitch2"
+              ></label>
+            </div>
+          </td>
+          <td>
+            <router-link :to="{ name: 'Registro' }">
+            <i class="fal fa-pencil d-inline"></i>
+            </router-link>
+            <i class="fal fa-trash-alt d-inline ml-2"></i>
+          </td>
+        </tr>
+      </tbody>
+    </table> -->
+  </div>
+</template>
+
+<script>
+import UsuarioMetodos from "../../domain/User/UsuarioMetodos";
+import TabelaServidores from '../../components/DataTables/TabelaServidores.vue'
+import PagesSubHeader from '../../components/subheader/PagesSubHeader.vue'
+export default {
+  components: {
+    PagesSubHeader,
+    TabelaServidores,
+  },
+  name: 'ListaServidores',
+  data() {
+    return {
+      filter: "",
+      usuarios: [],
+      msg: "",
+    };
+  },
+  methods: {},
+  created() {
+    this.service = new UsuarioMetodos(this.$resource);
+    this.service.list().then(
+        (usuarios) => (this.usuarios = usuarios),
+        (err) => {
+          console.log(err);
+          this.msg = err.message;
+        }
+    );
+  },
+  computed: {
+    UsuarioFiltrado() {
+      if (this.filter) {
+        let exp = new RegExp(this.filter.trim(), "i");
+        return this.usuarios.filter((usuario) => exp.test(usuario.user));
+      } else {
+        return this.usuarios;
+      }
+    },
+  },
+};
+</script>
+
+<style >
+/*.socorro {*/
+/*  display: flex;*/
+/*  justify-content: end;*/
+/*}*/
+/*.ajuda {*/
+/*  align-items: flex-end;*/
+/*}*/
+/*.bar {*/
+/*  width: 955.5px !important;*/
+/*}*/
+/*.title {*/
+/*  font-family: Arial, Helvetica, sans-serif;*/
+/*  text-transform: uppercase;*/
+/*  margin-left: 30px;*/
+/*  margin-top: 15px;*/
+/*}*/
+/*.register-form {*/
+/*  margin: 50px 65px 40px;*/
+/*}*/
+.panel-content{
+  overflow: auto;
+}
+.panel .panel-container .panel-content {
+  padding: 0;
+}
+.card-body{
+  padding: 5px;
+  height: 50px;
+  width: 0;
+  border: 0;
+  color: #ffffff;
+}
+.card{
+  box-shadow: none;
+  border: none;
+}
+.dow-color{
+  background-color: #1a7f37 !important;
+}
+</style>
